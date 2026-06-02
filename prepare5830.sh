a38c134cb2d1f1e4d3f7a824c40ed87e23471d3a
Trayansh Dewangan <68157983+trayansh@users.noreply.github.com>
2026-06-02 13:58:05 +0530

    Create prepare5830.sh

---
+++ prepare5830.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get install git-core gitk git-gui curl lvm2 thin-provisioning-tools python3-pkg-resources python3-virtualenv python3-oauth2client xz-utils python3.11
+mkdir openFyde/src && cd openFyde/src
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$PATH:${PWD}/depot_tools"
+repo init -u https://chromium.googlesource.com/chromiumos/manifest.git -b release-R132-16093.B
+repo sync -j$(nproc)
+mkdir -p .repo/local_manifests
+cd .repo/local_manifests
+git clone https://github.com/openFyde/manifest.git -b r132-openFyde .
+cd ../..
+repo sync -j$(nproc)
+cros_workon --board=amd64-openfyde start sys-kernel/chromeos-kernel-5_4
+cd /mnt/host/source/src/third_party/kernel/v5.4
+./chromeos/scripts/kernelconfig editconfig
+
+
+
+
